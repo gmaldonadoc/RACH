e5777c4831ee4f47c22202f0570a8f137286d153
gmaldonadoc <72625906+gmaldonadoc@users.noreply.github.com>
2024-06-17 15:14:52 -0300

    Add files via upload

---
+++ Debug/src/componentManagers/InformationManager.d
@@ -0,0 +1,5 @@
+src/componentManagers/InformationManager.d: \
+ ../src/componentManagers/InformationManager.cpp \
+ ../src/componentManagers/InformationManager.h
+
+../src/componentManagers/InformationManager.h:
---
+++ Debug/src/componentManagers/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/componentManagers/InformationManager.cpp \
+../src/componentManagers/FlowsManager.cpp \
+../src/componentManagers/FrameManager.cpp \
+../src/componentManagers/NetworkManager.cpp 
+
+OBJS += \
+./src/componentManagers/InformationManager.o \
+./src/componentManagers/FlowsManager.o \
+./src/componentManagers/FrameManager.o \
+./src/componentManagers/NetworkManager.o 
+
+CPP_DEPS += \
+./src/componentManagers/InformationManager.d \
+./src/componentManagers/FlowsManager.d \
+./src/componentManagers/FrameManager.d \
+./src/componentManagers/NetworkManager.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/componentManagers/%.o: ../src/componentManagers/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
