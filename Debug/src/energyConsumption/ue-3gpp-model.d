37001a6ee5947ce2170f89d4a06b6e4682a691ba
gmaldonadoc <72625906+gmaldonadoc@users.noreply.github.com>
2024-06-17 15:17:11 -0300

    Add files via upload

---
+++ Debug/src/energyConsumption/ue-3gpp-model.d
@@ -0,0 +1,152 @@
+src/energyConsumption/ue-3gpp-model.d: \
+ ../src/energyConsumption/ue-3gpp-model.cpp \
+ ../src/energyConsumption/ue-3gpp-model.h \
+ ../src/energyConsumption/ue-consumption-model.h \
+ ../src/energyConsumption/../drx/drx-manager.h \
+ ../src/energyConsumption/../drx/../device/NetworkNode.h \
+ ../src/energyConsumption/../drx/../device/../networkTopology/Cell.h \
+ ../src/energyConsumption/../drx/../device/../networkTopology/../core/cartesianCoodrdinates/CartesianCoordinates.h \
+ ../src/energyConsumption/../drx/../device/../networkTopology/Femtocell.h \
+ ../src/energyConsumption/../drx/../device/../networkTopology/Cell.h \
+ ../src/energyConsumption/../drx/../device/IPClassifier/Classifier.h \
+ ../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/Packet.h \
+ ../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/Header.h \
+ ../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/../protocols/TransportProtocol.h \
+ ../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/PacketTAGs.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/protocols/TransportProtocol.h \
+ ../src/energyConsumption/../drx/../device/../mobility/Mobility.h \
+ ../src/energyConsumption/../drx/../device/../mobility/../core/cartesianCoodrdinates/CartesianCoordinates.h \
+ ../src/energyConsumption/../drx/../device/../mobility/RandomDirection.h \
+ ../src/energyConsumption/../drx/../device/../mobility/Mobility.h \
+ ../src/energyConsumption/../drx/../device/../mobility/RandomWalk.h \
+ ../src/energyConsumption/../drx/../device/../mobility/RandomWaypoint.h \
+ ../src/energyConsumption/../drx/../device/../mobility/ConstantPosition.h \
+ ../src/energyConsumption/../drx/../device/../mobility/Manhattan.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/protocol-stack.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/packet/Packet.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/packet/Header.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/packet/PacketTAGs.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/protocols/TransportProtocol.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/nas/nas-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/rrc/rrc-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/rlc/rlc-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/rlc/../../load-parameters.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/mac-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/enb-mac-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/mac-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/simulator.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/event.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/calendar.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/make-event.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/henb-mac-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/enb-mac-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/ue-mac-entity.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/event.h \
+ ../src/energyConsumption/../drx/../device/../protocolStack/pdcp/pdcp-entity.h \
+ ../src/energyConsumption/../device/UserEquipment.h \
+ ../src/energyConsumption/../device/NetworkNode.h \
+ ../src/energyConsumption/../device/../drx/drx-manager.h \
+ ../src/energyConsumption/../device/../energyConsumption/ue-energy-model.h \
+ ../src/energyConsumption/../device/../energyConsumption/../device/NetworkNode.h \
+ ../src/energyConsumption/../device/../energyConsumption/baterry-model.h \
+ ../src/energyConsumption/../device/../energyConsumption/../device/UserEquipment.h
+
+../src/energyConsumption/ue-3gpp-model.h:
+
+../src/energyConsumption/ue-consumption-model.h:
+
+../src/energyConsumption/../drx/drx-manager.h:
+
+../src/energyConsumption/../drx/../device/NetworkNode.h:
+
+../src/energyConsumption/../drx/../device/../networkTopology/Cell.h:
+
+../src/energyConsumption/../drx/../device/../networkTopology/../core/cartesianCoodrdinates/CartesianCoordinates.h:
+
+../src/energyConsumption/../drx/../device/../networkTopology/Femtocell.h:
+
+../src/energyConsumption/../drx/../device/../networkTopology/Cell.h:
+
+../src/energyConsumption/../drx/../device/IPClassifier/Classifier.h:
+
+../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/Packet.h:
+
+../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/Header.h:
+
+../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/../protocols/TransportProtocol.h:
+
+../src/energyConsumption/../drx/../device/IPClassifier/../../protocolStack/packet/PacketTAGs.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/protocols/TransportProtocol.h:
+
+../src/energyConsumption/../drx/../device/../mobility/Mobility.h:
+
+../src/energyConsumption/../drx/../device/../mobility/../core/cartesianCoodrdinates/CartesianCoordinates.h:
+
+../src/energyConsumption/../drx/../device/../mobility/RandomDirection.h:
+
+../src/energyConsumption/../drx/../device/../mobility/Mobility.h:
+
+../src/energyConsumption/../drx/../device/../mobility/RandomWalk.h:
+
+../src/energyConsumption/../drx/../device/../mobility/RandomWaypoint.h:
+
+../src/energyConsumption/../drx/../device/../mobility/ConstantPosition.h:
+
+../src/energyConsumption/../drx/../device/../mobility/Manhattan.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/protocol-stack.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/packet/Packet.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/packet/Header.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/packet/PacketTAGs.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/protocols/TransportProtocol.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/nas/nas-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/rrc/rrc-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/rlc/rlc-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/rlc/../../load-parameters.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/mac-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/enb-mac-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/mac-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/simulator.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/event.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/calendar.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/make-event.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/henb-mac-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/enb-mac-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/ue-mac-entity.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/mac/../../core/eventScheduler/event.h:
+
+../src/energyConsumption/../drx/../device/../protocolStack/pdcp/pdcp-entity.h:
+
+../src/energyConsumption/../device/UserEquipment.h:
+
+../src/energyConsumption/../device/NetworkNode.h:
+
+../src/energyConsumption/../device/../drx/drx-manager.h:
+
+../src/energyConsumption/../device/../energyConsumption/ue-energy-model.h:
+
+../src/energyConsumption/../device/../energyConsumption/../device/NetworkNode.h:
+
+../src/energyConsumption/../device/../energyConsumption/baterry-model.h:
+
+../src/energyConsumption/../device/../energyConsumption/../device/UserEquipment.h:
